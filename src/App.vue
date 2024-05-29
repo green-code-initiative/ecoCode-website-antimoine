4bc9a3366457ef4940ec3e6b312db327dfddb613
apej06 <alexandre.jullien06@gmail.com>
2024-05-29 16:09:13 +0200

    change App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { RouterView } from 'vue-router';
+import { RouterView } from 'vue-router'
 import Header from '@/components/shared/layout/AppHeader.vue';
 import Footer from '@/components/shared/layout/AppFooter.vue';
-
-onMounted(() => {
-  try {
-    let test = undefined;
-    console.log(test.property);
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-});
 </script>
 
 <template>
