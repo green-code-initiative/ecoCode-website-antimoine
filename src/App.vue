892b5e60c626de2b9652f7991929f878968d068c
apej06 <alexandre.jullien06@gmail.com>
2024-05-29 14:40:15 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
 import Header from '@/components/shared/layout/AppHeader.vue';
 import Footer from '@/components/shared/layout/AppFooter.vue';
+
+onMounted(() => {
+  try {
+    let test = undefined;
+    alert('toto');
+    console.log('toto');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+});
 </script>
 
 <template>
